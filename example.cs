67b9b6eacc88aa7b1c401088b65e7ee987e88fbc
samudrak99x <samudrak@99x.io>
2025-02-14 08:10:10 +0530

    reverted

--- example.cs
+++ example.cs
@@ -1,19 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        var numbers = new List<int> { 1, 2, 3, 4, 5 };
-
+        List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+        
         Console.WriteLine("Original List:");
-        numbers.ForEach(Console.WriteLine);
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
 
         Console.WriteLine("Doubled List:");
-        var doubledNumbers = numbers.Select(num => num * 2);
-        foreach (var num in doubledNumbers)
+        List<int> doubledNumbers = new List<int>();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            doubledNumbers.Add(numbers[i] * 2);
+        }
+
+        foreach (int num in doubledNumbers)
         {
             Console.WriteLine(num);
         }
